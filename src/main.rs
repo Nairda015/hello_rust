fa4053792d80b8b656485a41c87e1e98dd59e38e
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2023-05-08 20:04:52 +0200

    fundamentals

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,21 @@
-use colored::*;
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
-
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("Secret number is between 1 and 100.");
+    let x = 5;
+    println!("The value of x is: {}", x);
+    let x = "six";
+    println!("The value of x is: {}", x);
 
-    loop {
-        println!("Please input your guess.");
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line!");
+    //const SUB_COUNT: u32 = 100_000;
+    let sum = my_func(1, 2);
+    println!("Value of x + y: {}", sum);
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("{}", "Too small!".red()),
-            Ordering::Greater => println!("{}", "Too big!".red()),
-            Ordering::Equal => {
-                println!("{}", "You win!".green());
-                break;
-            }
-        }
+    let coll = [1, 2, 3, 4];
+    for e in coll.iter() {
+        println!("{}", e);
     }
+}
+
+fn my_func(x: i32, y: i32) -> i32 {
+    println!("Value of x: {}", x);
+    println!("Value of y: {}", y);
+    x + y
 }
