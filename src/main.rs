4cd77fa7de815fb4354c032fe70e149a621bc50e
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2023-05-09 20:03:13 +0200

    lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello world!")
+    println!("Hello world!");
+
+    let s1 = String::from("abc");
+    let s2 = String::from("abcd");
+
+    let result = longest(s1.as_str(), s2.as_str());
+    println!("{}", result);
+
+    let s1 = String::from("abc");
+    let result;
+    {
+        let s2 = String::from("abcd");
+        result = longest2(s1.as_str(), s2.as_str());
+    }
+    println!("{}", result);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest2<'a>(x: &'a str, y: &str) -> &'a str {
+    x
 }
