953fe8bca05248235fb7e3c7bb3bd9b906ef52d9
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2023-05-12 19:29:17 +0200

    migrep-02

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,17 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+use migrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem while parsing arguments: {}", err);
+        eprintln!("Problem while parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("{:?}", args);
-    println!("Search for: {}", config.query);
-    println!("In file: {}", config.filename);
-
-    if let Err(e) = run(config){
-        println!("Application error: {}", e);
+    if let Err(e) = migrep::run(config){
+        eprintln!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.filename)?;
-
-    println!("Content: \n{}", content);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enought arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
