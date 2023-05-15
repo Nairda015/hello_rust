8742121bb496a71f5cf120e81280084aa6d92c5c
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2023-05-15 19:41:54 +0200

    state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use hello_rust::Post;
+
 fn main() {
-    println!("Hello world!")
+    let mut post = Post::new();
+    post.add_text("I ate a salad for lunch today");
+
+    let pending_post = post.request_review();
+
+    let post = pending_post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
