4441fb468f5d30926ed491d590be85ec6d9420d7
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2023-05-15 19:03:16 +0200

    gui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use hello_rust::{Button, Draw, Screen};
+
 fn main() {
-    println!("Hello world!")
+    let select_box = Box::new(SelectBox {
+        width: 10,
+        height: 5,
+        options: vec![
+            String::from("yes"),
+            String::from("no"),
+            String::from("maybe"),
+        ],
+    });
+
+    let button = Box::new(Button {
+        width: 10,
+        height: 5,
+        label: String::from("ok"),
+    });
+
+    let screen = Screen {
+        components: vec![select_box, button],
+    };
+
+    screen.run()
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("{:?}", self.options)
+    }
 }
