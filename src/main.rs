b33571af21c05cd562ac809d84491aa14111db30
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2023-05-16 11:52:14 +0200

    slow_server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::{fs, thread};
+
 fn main() {
-    println!("Hello world!")
+    println!("Hello world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established");
+        hendlde_connection(stream);
+    }
+}
+
+fn hendlde_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+    //    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let (status_line, page_name) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let content = fs::read_to_string(page_name).unwrap();
+    let response = format!(
+        "{}\r\nContent-Lenght: {}\r\n\r\n{}",
+        status_line,
+        content.len(),
+        content
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
